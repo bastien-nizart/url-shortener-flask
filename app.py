aec50f2968d90c6894fb699f8323c61434b58967
Bastien <bastien@nizart.me>
2022-10-31 17:24:49 +0100

    added automatic table creation

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
+import sqlite3
+from sqlite3 import OperationalError
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
+def table_creation():
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE hash(hash text primary key, url text)''')
+    con.commit()
+
+
 @app.route('/')
-def hello_world():  # put application's code here
+def main():
+    try:
+        table_creation()
+    except OperationalError:
+        print("log : table already exist")
+
     return 'Hello World!'
 
 
